01b70a9906b1398fb90da218b07377c289b93d9c
unJZloty <zolotenkiy@gmail.com>
2024-08-27 11:24:34 +0300

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -68,7 +68,7 @@ class Cube(Figure):
     def get_volume(self):
         return self.get_sides()[0] ** 3
 
-circle1 = Circle((200, 200, 100), 10)
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
 # Проверка на изменение цветов:
