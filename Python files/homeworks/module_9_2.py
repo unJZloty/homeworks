7b021839f913472d02fbee3dc3445c7162167cbb
unJZloty <zolotenkiy@gmail.com>
2024-09-10 22:46:30 +0300

    10.09.2024

---
+++ Python files/homeworks/module_9_2.py
@@ -0,0 +1,9 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+first_result = [len(s) for s in first_strings if len(s) >= 5]
+second_result = [(fs, ss) for fs in first_strings for ss in second_strings if len(fs) == len(ss)]
+third_result = {s: len(s) for s in first_strings + second_strings if len(s) % 2 == 0}
+
+print(first_result)
+print(second_result)
+print(third_result)
