35a680602afaa10d5914a92e09d0956dcd3065f5
unJZloty <zolotenkiy@gmail.com>
2024-09-20 17:51:29 +0300

    20.09.2024

--- Python files/homeworks/module_10_3.py
+++ Python files/homeworks/module_10_3.py
@@ -1,21 +1,21 @@
-import threading
-import random
 import time
+import random
+from threading import Thread, Lock
 
 class Bank:
     def __init__(self):
-        self.balance = 0
-        self.lock = threading.Lock()
+        self.balance = 500
+        self.lock = Lock()
 
     def deposit(self):
         for _ in range(100):
             amount = random.randint(50, 500)
             self.balance += amount
-            print(f"Пополнение: {amount}. Баланс: {self.balance}")
-
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-        time.sleep(0.001)
+            print(f"Пополнение: {amount}. Баланс: {self.balance}")
+
+            time.sleep(0.001)
 
     def take(self):
         for _ in range(100):
@@ -29,10 +29,12 @@ class Bank:
                 print("Запрос отклонён, недостаточно средств")
                 self.lock.acquire()
 
+            time.sleep(0.001)
+
 bk = Bank()
 
-th1 = threading.Thread(target=bk.deposit)
-th2 = threading.Thread(target=bk.take)
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
 
 th1.start()
 th2.start()
