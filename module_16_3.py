8ddc0b8f6c3ffd99256e7f3ca46ae6e8f67d7ab9
unJZloty <zolotenkiy@gmail.com>
2025-01-14 10:12:54 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -10,13 +10,15 @@ async def get_users():
     return users
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: int) -> str:
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', examples='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', examples=24)]) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f'Имя: {username}, возраст: {age}'
     return f"User {current_index} is registered"
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int):
+async def update_user(user_id: int, username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', examples='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', examples=24)]):
     users[str(user_id)] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} has been updated'
 
--- module_16_4.py
+++ module_16_4.py
@@ -1,40 +1,53 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
 from pydantic import BaseModel
-from typing import List
-
-app = FastAPI()
 
 users = []
+
+app = FastAPI()
 
 class User(BaseModel):
     id: int
     username: str
     age: int
 
-@app.get("/users", response_model=List[User])
-def get_users():
+@app.get('/users')
+async def get_users() -> List[User]:
     return users
 
-@app.post("/user/{username}/{age}", response_model=User)
-def create_user(username: str, age: int):
-    user_id = users[-1].id + 1 if users else 1
-    new_user = User(id=user_id, username=username, age=age)
-    users.append(new_user)
-    return new_user
+@app.post('/user/{username}/{age}')
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', examples='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', examples=24)]) -> User:
+    try:
+        if users:
+             user_id = users[-1].id + 1
+        else:
+            user_id = 1
 
-@app.put("/user/{user_id}/{username}/{age}", response_model=User)
-def update_user(user_id: int, username: str, age: int):
+        new_user = User(id=user_id, username=username, age=age)
+        users.append(new_user)
+        return new_user
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(description='Enter user ID', examples=1)],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', examples='UrbanProfi')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', examples=28)]) -> User:
     for user in users:
         if user.id == user_id:
             user.username = username
             user.age = age
             return user
-    raise HTTPException(status_code=404, detail="User was not found")
+        else:
+            raise HTTPException(status_code=404, detail="User was not found")
 
-@app.delete("/user/{user_id}", response_model=User)
-def delete_user(user_id: int):
-    for user in users:
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(description='Enter user ID', examples=1)]) -> User:
+    for i, user in enumerate(users):
         if user.id == user_id:
-            users.remove(user)
-            return user
+            deleted_user = users.pop(i)
+            return deleted_user
+
     raise HTTPException(status_code=404, detail="User was not found")
